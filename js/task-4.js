5c56922e0dde4c06780ef838a4de5bac6d957de7
YaninaBrizg <yanina.brizg@gmail.com>
2025-05-19 14:31:09 +0200

    change of HM7

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault(); // ❗️зупиняємо перезавантаження сторінки
+
+  const { email, password } = form.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  if (emailValue === '' || passwordValue === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData); // ✅ виводимо об'єкт у консоль
+  form.reset(); // ✅ очищаємо форму
+});
